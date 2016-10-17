7babf8d4b24c3adf31d2405f57b3b3156896a32c
maxboyko <maxboykomax@gmail.com>
2016-10-17 22:01:24 +0000

    Fix navbar to top of the page and move tagline to masthead.

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,6 @@ gulp.task('inject', () => {
 gulp.task('compile:sass', () => {
     return gulp.src('./sass/main.scss')
         .pipe($.sass().on('error', $.sass.logError))
-        .pipe($.autoprefixer({browsers: ['last 2 version', '>5%']}))
+        .pipe($.autoprefixer({browsers: ['last 2 version', '>1%']}))
         .pipe(gulp.dest('./public/css'));
 });
